ff5a2b5a54e2efa450e7f9a1ab67da467587fb0d
kristaps.taube <kristaps.taube@printful.com>
2020-07-16 19:10:29 +0300

    Callable test command

--- app/Config/CommandConfigurators/TestCommandConfigurator.php
+++ app/Config/CommandConfigurators/TestCommandConfigurator.php
@@ -10,6 +10,17 @@ class TestCommandConfigurator implements CommandConfiguratorInterface
 {
     public function configureCommands(CommandRunner $commandRunner)
     {
+        // regular command handler
         $commandRunner->addCommand('test', TestCommandHandler::class);
+
+        // callable command handler
+        $commandRunner->addCommand('callable',
+            function ($foo, $bar = null) {
+                echo 'This is callable command with foo = ' . print_r($foo, true) . PHP_EOL;
+                echo 'Bar is ';
+                var_dump($bar);
+                echo PHP_EOL;
+            }
+        );
     }
 }
